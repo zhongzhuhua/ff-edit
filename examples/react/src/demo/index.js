abdd39c56ee59415b1c49f3e5463df643e2ccbb5
zhongzhuhua <672986035@qq.com>
2017-05-03 16:36:55 +0800

    router

--- examples/react/src/demo/index.js
+++ examples/react/src/demo/index.js
@@ -1,3 +1,7 @@
+/** 
+ * 总入口页
+ * @author all
+ */
 import React  from 'react';
 import {
   render
@@ -13,52 +17,11 @@ import Temp from './views/temp';
 function App() {
   return (
     <HashRouter>
-      <div>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/temp" component={Temp} />
-        </Switch>
-      </div>
+      <Switch>
+         <Route exact path="/" component={Home} />
+         <Route path="/temp" component={Temp} />
+      </Switch>
     </HashRouter>
   );
 }
 render(<App />, document.getElementById('root'));
-
-// /** 
-//  * 总入口页
-//  * @author all
-//  */
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
-// import {
-//   BrowserRouter as Router,
-//   Route
-// } from 'react-router-dom';
-
-// // 路由器
-// import Home from './views/home';
-// import Temp from './views/temp';
-
-// // router entry
-// class App extends React.Component {
-//   render() {
-//     document.title = this.props.children.props.route.title || '';
-//     return (
-//       <div>
-//         {this.props.children}
-//       </div>
-//     );
-//   };
-// };
-
-// // 路由配置
-// ReactDOM.render(
-//   <Router>
-//     <Route component={App}>
-//       <Route path="/" component={Home} />
-//       <Route path="/temp" component={Temp} />
-//     </Route>
-//   </Router>,
-//   document.getElementById('root')
-// );
